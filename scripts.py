91f3510a7b8a8d1cae0a5e3535f23763e4585fd1
Steven <steven.fleuren@gmail.com>
2017-11-08 12:22:06 +0100

    scriptupdate

--- scripts.py
+++ scripts.py
@@ -1,4 +1,3 @@
-from collections import deque
 import graphs
 
 n = 11
@@ -6,11 +5,19 @@ m = 13
 edges = {0: [1, 4], 1: [0, 2], 2: [1, 3, 5], 3: [2, 6], 4: [0, 5], 5: [2, 4, 6, 9], 6: [3, 5, 7], 7: [6], 8: [9],
          9: [5, 8, 10], 10: [9]}
 G = graphs.Graphs(edges)
-# G.findArticulationPoints()
-# for u in G.articulationPoints:
-#     print(u)
-# G.constructBlocks()
+G.constructBlocks()
 
+print("Articulation points:")
+for i in G.articulationPoints:
+    print(i)
+print()
+print("Components:")
 for i in G.blockContains:
-    print('component: '+ str(i))
-T = G.constructWeightedBlockTree()
+    print(i)
+print()
+for i in range(n):
+    print("Vertex: " + str(i) + ", in component(s): " + str(G.blockOfU[i]))
+print()
+G.brandes()
+for i in range(n):
+    print(G.bc[i])

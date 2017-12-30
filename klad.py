199e632e9b552e42a940b8b7663c914d6edfcf0f
Envest0770 <steven.fleuren@gmail.com>
2017-12-30 17:19:16 +0100

    Seems to be working

--- klad.py
+++ klad.py
@@ -5,3 +5,55 @@ edges2 = [[0], [0, 1, 2], [1]]
 G = graphs.connectTree(edges1, edges2)
 print(G.edges)
 
+"""
+def connectTree(edges1, edges2):
+    length = len(edges1)
+    for u in range(length):
+        for i in range(len(edges1[u])):
+            edges1[u][i] += length
+    edges1 += edges2
+    return Graphs(edges1)
+
+"""
+
+
+"""
+n = self.n
+counter = n
+self.dfs(start, True)
+highest = self.d.copy()
+dToVertex = [0] * n  # Avoids O(n) searches later
+for u in range(n):
+    dToVertex[self.d[u]] = u
+while counter > 0:
+    counter -= 1
+    u = dToVertex[counter]
+    for v in self.edges[u]:
+        if v == self.pi[u]:  # v is parent in dfs tree
+            pass
+        elif self.d[v] < self.d[u]:  # backedge
+            if self.d[v] < highest[u]:
+                highest[u] = self.d[v]
+        else:  # child
+            if highest[v] < highest[u]:
+                highest[u] = highest[v]
+u = dToVertex[0]
+children = 0
+for v in self.edges[u]:
+    if u == self.pi[v]:
+        children += 1
+if children > 1:
+    self.articulationPoints.append(u)
+    self.blockOfU[u] = []
+else:
+    self.addComponent(u)
+for i in range(1, n):
+    u = dToVertex[i]
+    for v in self.edges[u]:
+        if u == self.pi[v]:  # v is a child of u
+            if highest[v] >= i:  # This means u is an articulation point
+                if not self.isArticulationPoint(u):
+                    # This means it's the first time it's determined that u is an articulation point
+                    self.blockOfU[u] = []
+                    self.articulationPoints.append(u)
+"""
--- scripts.py
+++ scripts.py
@@ -4,6 +4,10 @@ import graphs
 
 edges = [[1, 4], [0, 2], [1, 3, 5], [2, 6], [0, 5], [2, 4, 6, 9], [3, 5, 7], [6], [9], [5, 8, 10], [9]]
 # edges = [[1, 3, 4], [0, 2, 3], [1, 4], [0, 1], [0, 2]]
+# edges = [[1], [0, 2], [1, 3], [2]]
+# edges = [[1, 3], [0, 2], [1, 3], [0, 2]]
+# edges = [[1, 4], [0, 2, 3], [1, 4], [1], [0, 2]]
+# edges = [[1, 3], [0, 2, 4], [1, 3, 5], [0, 2], [1], [2, 6], [5]]
 n = len(edges)
 
 G = graphs.Graphs(edges)
@@ -15,7 +19,7 @@ for i in G.articulationPoints:
 print()
 G.constructBlocks()
 print("Components:")
-for i in G.blockContains[1:]:
+for i in G.blockContains:
     print(i)
 print()
 for i in range(n):
@@ -23,4 +27,8 @@ for i in range(n):
 print()
 G.brandes()
 for i in range(n):
-    print(G.bc[i])
+    print("BC(" + str(i) + ") = " + str(G.bc[i]))
+print()
+G.BCBCC()
+for i in range(n):
+    print("BC(" + str(i) + ") = " + str(G.bc[i]))
